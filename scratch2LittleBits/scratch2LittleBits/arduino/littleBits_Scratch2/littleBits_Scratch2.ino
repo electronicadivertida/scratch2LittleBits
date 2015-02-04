1e4d41e140e609873aff152cfafb0018b2107f2c
electronicadivertida <eduardo@electronicadivertida.com>
2015-02-04 04:15:57 +0100

    Parpadeo cuando el led esta apagado, era fallo de sincronizacion con puerto serie, se añade digito control

--- scratch2LittleBits/scratch2LittleBits/arduino/littleBits_Scratch2/littleBits_Scratch2.ino
+++ scratch2LittleBits/scratch2LittleBits/arduino/littleBits_Scratch2/littleBits_Scratch2.ino
@@ -9,21 +9,24 @@
 const int READ_PINS = 1;
 const int WRITE_ANALOG = 2;
 const int WRITE_DIGITAL = 3;
+const int BYTE_CONTROL = 30;
 
 // Analog input smoothing
 // http://arduino.cc/en/Tutorial/Smoothing
 
-const int sensorChannels = 8;
-const int maxNumReadings = 30;
+const int sensorChannels = 20;
+const int maxNumReadings = 7;
 
 int smoothingValues[sensorChannels][maxNumReadings];
 int smoothingIndex[sensorChannels];
 int smoothingTotal[sensorChannels];
 
 
-int averageA1 = 0;
-int averageA0 = 0;
+int averageA1;
+int averageA0;
 
+
+int mlUltValor[3];
 
 void setup() {
   
@@ -34,11 +37,19 @@ void setup() {
   pinMode(1, OUTPUT);
   pinMode(5, OUTPUT);
   pinMode(9, OUTPUT);
-  setupSmoothing();  
+
+  mlUltValor[0]=-1;
+  mlUltValor[1]=-1;
+  mlUltValor[2]=-1;
+  
+  setupSmoothing();
 
 }
 
 void setupSmoothing() {
+     averageA1 = 0;
+     averageA0 = 0;
+  
      for(int i = 0; i < sensorChannels; i++) {
        for(int j = 0 ; j < maxNumReadings ; j++) {
          smoothingValues[i][j]=0;
@@ -55,42 +66,54 @@ void loop() {
   // Check if there are bytes on the Serial port
   if (Serial.available() > 0) {
     
-    // Get first available byte
+    // 1 byte control
     incomingByte = Serial.read();
-    
-    if (incomingByte == READ_PINS) {
-    
-      // Read digital pin 0
-      Serial.write(digitalRead(0));
+    if(BYTE_CONTROL == incomingByte){
+      // Get first available byte
+      incomingByte = Serial.read();
       
-      // Get averages for analog pins 0 and 1
-      Serial.write(averageA0);
-      Serial.write(averageA1);
+      if (incomingByte == READ_PINS) {
       
-    } else if (incomingByte == WRITE_ANALOG) {
-    
-      // Next byte from Scratch is pin number
-      int outputPin = Serial.read();
+        // Read digital pin 0
+        Serial.write(BYTE_CONTROL);
+        Serial.write(digitalRead(0));
+        
+        // Get averages for analog pins 0 and 1
+        Serial.write(averageA0);
+        Serial.write(averageA1);
+        
+      } else if (incomingByte == WRITE_ANALOG) {
       
-      // Next byte from Scratch is pin value
-      int outputVal = Serial.read();
+        // Next byte from Scratch is pin number
+        int outputPin = Serial.read();
+        
+        // Next byte from Scratch is pin value
+        int outputVal = Serial.read();
+        
+        if(outputPin==5 && mlUltValor[1]!=outputVal){
+            mlUltValor[1]=outputVal;
+            analogWrite(outputPin, outputVal);
+        }
+        if(outputPin==9 && mlUltValor[2]!=outputVal){
+            mlUltValor[2]=outputVal;
+            analogWrite(outputPin, outputVal);
+        }
+      } else if (incomingByte == WRITE_DIGITAL) {
       
-      analogWrite(outputPin, outputVal);
-    
-    } else if (incomingByte == WRITE_DIGITAL) {
-    
-      // Next byte from Scratch is pin number
-      int outputPin = Serial.read();
+        // Next byte from Scratch is pin number
+        int outputPin = Serial.read();
+        
+        // Next byte from Scratch is pin value
+        int outputVal = Serial.read();
+        
+        if(mlUltValor[0]!=outputVal){
+           mlUltValor[0]=outputVal;
+           digitalWrite(outputPin, outputVal);
+        }
       
-      // Next byte from Scratch is pin value
-      int outputVal = Serial.read();
-      
-      digitalWrite(outputPin, outputVal);
-    
+      } 
     }
-    
   }
-  
   // Analog input smoothing
   // http://arduino.cc/en/Tutorial/Smoothing
   averageA0 = readAnalogPort(A0);
@@ -100,24 +123,24 @@ void loop() {
 }
 
 
-int smoothingValue(int channel, int value, int numReadings) {
+int smoothingValue(int channel, int value) {
     int total;
     int index = smoothingIndex[channel];
     total = smoothingTotal[channel] - smoothingValues[channel][index];
     smoothingValues[channel][index] = value;
     smoothingTotal[channel] = total + value;
     smoothingIndex[channel]++;
-    if(smoothingIndex[channel] >=numReadings) {
+    if(smoothingIndex[channel] >=maxNumReadings) {
       smoothingIndex[channel]=0;
     }
 
-    return int(round(smoothingTotal[channel] / (numReadings)));
+    return int(round(smoothingTotal[channel] / (maxNumReadings)));
 }
 
 int readAnalogPort(int plPin) {
   int value;
   value = analogRead(plPin);
-  //value = smoothingValue(plPin, value, 5);
+  value = smoothingValue(plPin, value);
   if (value == 1022) value = 1023;
   return value/4;
 }
