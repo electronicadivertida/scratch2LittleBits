9db87397779c6cc00ddd602a526961cac3816ff1
electronicadivertida <eduardo@electronicadivertida.com>
2015-08-26 02:57:16 +0200

    Enlace con Android

---
+++ scratch2LittleBits/scratch2LittleBitsAndroid/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/home/d/programas/electronicadivertida/scratch2/pluginLocal/scratch2LittleBits/scratch2LittleBits/scratch2LittleBitsAndroid/bin/AndroidManifest.xml : \
+/home/d/programas/electronicadivertida/scratch2/pluginLocal/scratch2LittleBits/scratch2LittleBits/scratch2LittleBitsAndroid/AndroidManifest.xml \
---
+++ scratch2LittleBits/scratch2LittleBitsAndroid/bin/littleBitsBluetooth-debug-unaligned.apk.d
@@ -0,0 +1,4 @@
+/home/d/programas/electronicadivertida/scratch2/pluginLocal/scratch2LittleBits/scratch2LittleBits/scratch2LittleBitsAndroid/bin/littleBitsBluetooth-debug-unaligned.apk : \
+/home/d/programas/electronicadivertida/scratch2/pluginLocal/scratch2LittleBits/scratch2LittleBits/scratch2LittleBitsAndroid/bin/littleBitsBluetooth.ap_ \
+/home/d/programas/electronicadivertida/scratch2/pluginLocal/scratch2LittleBits/scratch2LittleBits/scratch2LittleBitsAndroid/bin/classes.dex \
+/home/d/programas/electronicadivertida/scratch2/pluginLocal/scratch2LittleBits/scratch2LittleBits/scratch2LittleBitsAndroid/libs/android-support-v4.jar \
